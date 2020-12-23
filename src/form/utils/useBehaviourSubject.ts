cc1b5565a77e8e15a1e5fb53e90206c4dd36231d
Ivan Kasenkov <kacenkov7@gmail.com>
2020-12-23 20:06:30 +0300

    fix useBehaviourSubject

--- src/form/utils/useBehaviourSubject.ts
+++ src/form/utils/useBehaviourSubject.ts
@@ -1,11 +1,16 @@
-import { useLayoutEffect, useState } from 'react';
+import { Reducer, useLayoutEffect, useReducer } from 'react';
 import { BehaviourSubject } from './behaviourSubject';
 
-export const useBehaviourSubject = <T>(s: BehaviourSubject<T>): T => {
-  const [value, setValue] = useState<T>(s.value);
+export const useBehaviourSubject = <T>(subject: BehaviourSubject<T>): T => {
+  const [value, next] = useReducer<Reducer<T, T>>(
+    (_, action) => action,
+    subject.value,
+  );
+
   useLayoutEffect(() => {
-    const next = (v: T) => setValue(typeof v === 'function' ? () => v : v);
-    return s.subscribe({ next }).unsubscribe;
-  }, [s]);
+    next(subject.value);
+    return subject.subscribe({ next }).unsubscribe;
+  }, [subject]);
+
   return value;
 };
